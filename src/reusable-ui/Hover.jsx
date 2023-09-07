9a161e904220d927639c804e3376b8f3fc1ed7a4
Nahimana <mnahim024@gmail.com>
2023-09-08 01:27:32 +0200

    ✨ Projects : Add a block which appears when mouse is over a project

---
+++ src/reusable-ui/Hover.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Logo from "../reusable-ui/Logo";
+import githubLogo from "../assets/icons/logo-github.png";
+import details from "../assets/icons/details.png";
+
+const Hover = ({ link, handleClick }) => {
+  return (
+    <div className="hover">
+      <a href={link} target="_blank">
+        <div className="flex justify-center items-center text-gray-300 gap-2 md:hover:scale-110 md:transition-transform md:duration-300">
+          <Logo imageSource={githubLogo} alt={"github logo"} />
+          <p>Code</p>
+        </div>
+      </a>
+      <div
+        onClick={handleClick}
+        className="flex justify-center items-center text-gray-300 gap-2 cursor-pointer md:hover:scale-110 md:transition-transform md:duration-300"
+      >
+        <Logo imageSource={details} alt={"details icon"} />
+        <p>Détails</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hover;
